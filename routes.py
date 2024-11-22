2abd054a419365f1832f50d7eaf7f4ba01a69ed8
James Anderson <40646650@live.napier.ac.uk>
2024-11-22 18:37:14 +0000

    updated files

--- routes.py
+++ routes.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'index.html'
+def basicTemplate():
+    return render_template('baseTemplate.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
