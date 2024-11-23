5241faafdd9447c5d42caf55ab48271801abd866
James Anderson <40646650@live.napier.ac.uk>
2024-11-23 19:29:34 +0000

    added databse connection, init_db.y file

--- routes.py
+++ routes.py
@@ -1,5 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
 app = Flask(__name__)
+db_location = 'var/quizzle.db'
+
+def get_db():
+	db = getattr(g, 'db', None)
+	if db is None:
+		db = sqlite3.connect(db_location)
+		g.db = db
+	return db
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+	db = getattr(g, 'db', None)
+	if db is not None:
+		db.close()
+
+def init_db():
+	with app.app_context():
+		db = get_db()
+		with app.open_resource("quizzleSchema.sql", mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
 
 @app.route('/')
 def basicTemplate():
@@ -12,6 +35,8 @@ def login():
 @app.route('/sign-up')
 def signup():
         return render_template('sign-up.html')
+
+@app.route
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
